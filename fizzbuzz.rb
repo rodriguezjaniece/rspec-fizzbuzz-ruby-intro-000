4bfba87d9e3f1483882459389bd9de1c597d13fb
Janiece Rodriguez <rodriguezjaniece@yahoo.com>
2020-03-30 21:18:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,16 +3,16 @@
 
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-     puts "FizzBuzz"
+  "FizzBuzz"
 
   elsif number % 3 == 0
-      puts "Fizz"
+     "Fizz"
 
   elsif number % 5 == 0
-      puts "Buzz"
+     "Buzz"
 
   else (number % 3 != 0) || (number % 5 !=0)
-  puts nil
+   nil
 
  end
 end
